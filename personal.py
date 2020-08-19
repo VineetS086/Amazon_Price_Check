6abf1c4714ec3d67bdcb7eaa1490d27ff6e01c43
Sigma086 <68197705+VineetS086@users.noreply.github.com>
2020-08-19 15:56:25 +0530

    Add files via upload

---
+++ personal.py
@@ -0,0 +1,15 @@
+def user_agent():
+    return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+
+def credential():
+    email = 'ENTER EMAIL (SEND)'
+    password = 'PASSWORD'
+    
+    me = 'ENTER EMAIL (REC)'
+    
+    return (email, password,me)
+
+
+
+
+#add_new('https://www.amazon.in/gp/product/B07T81554H/ref=s9_acss_bw_cg_INele1_1b1_w?pf_rd_m=A1K21FY43GMZF8&pf_rd_s=merchandised-search-2&pf_rd_r=0PDXVM253HGDX47A4H9R&pf_rd_t=101&pf_rd_p=61344d7d-e9e2-4f34-993e-b8308f5b009c&pf_rd_i=976419031')
