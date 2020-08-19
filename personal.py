694084911f1196494cd8ae5524df7ae93d636afe
Sigma086 <68197705+VineetS086@users.noreply.github.com>
2020-08-19 15:57:55 +0530

    Update personal.py

--- personal.py
+++ personal.py
@@ -1,5 +1,5 @@
 def user_agent():
-    return 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'
+    return 'Your User Agent... Just Google it if U dont know'
 
 def credential():
     email = 'ENTER EMAIL (SEND)'
